c585ddc8dc9f37bf89a06089f9d89799779e1d40
Marisol-Zepeda <0200706@up.edu.mx>
2020-11-15 21:33:55 -0600

    0.3.0 First Person Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,47 @@
-﻿using UnityEngine;
+﻿ using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public InputAction wasd;
+    [SerializeField]
+    [Range(0.1f, 10)]
+    private float acceleration = 6f;
+    [SerializeField]
+    [Range(0.1f, 1)]
+    private float maxSpeed = 40;
+    [SerializeField]
+    [Range(0, 1)]
+    private float friccion = 0.9f;
+    [SerializeField]
+    [Range(0, 0.1f)]
+    private float tolerance = 0.01f;
+    
+    [SerializeField]  
+
     public Vector2 _inputVector;
-    public CharacterController controller;
+    private CharacterController controller;
+    private Vector3 _velocity;
+
+    public InputAction wasd;
     public float speed = 3.14f;
+
+
+    void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(_inputVector.x, 0, _inputVector.y);
+        _velocity += direction * acceleration * Time.deltaTime;
+        _velocity = Vector3.ClampMagnitude(_velocity, maxSpeed);
+        if(_velocity.magnitude<tolerance)
+        {
+            _velocity = Vector3.zero;
+        } else
+        {
+            controller.Move(_velocity);
+        }
+        _velocity *= friccion;
+    }
 
     void Start()
     {
