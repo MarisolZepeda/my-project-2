a8dee654c7736749887401440367f49fc2edf993
Marisol-Zepeda <0200706@up.edu.mx>
2020-11-21 12:06:43 -0600

    0.4.0.PlayerShoot

---
+++ Assets/PlayerWeapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform ShootPoint;
+    public float force = 30;
+    public float lifeTime = 3;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+    private void Fire()
+    {
+        var bullet = Instantiate(bulletPrefab, ShootPoint.position, Quaternion.identity);
+        var rigidBody = bullet.GetComponent<Rigidbody>();
+        rigidBody.AddForce(ShootPoint.forward * force, ForceMode.Impulse);
+        StartCoroutine(DestroyBulletAfterTime(bullet, lifeTime));
+
+    }
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+
+    }
+}
+;
